42cc455185159a095794cc21adf30f18afcf93d9
unknown <e.esposito.1520@vallauri.edu>
2021-11-11 11:05:50 +0100

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDatailComponent } from './recipes/recipe-datail/recipe-datail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDatailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeItemComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipes/recipe-datail/recipe-datail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-datail',
+  templateUrl: './recipe-datail.component.html',
+  styleUrls: ['./recipe-datail.component.css']
+})
+export class RecipeDatailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
